e69061cf4735152adf42386656cfbd061e4fa861
ariplayz <ari@arigreenecummings.com>
2025-03-17 13:23:12 -0700

    Committ

---
+++ delphigamerzsjfdslkfjcscharp/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace delphigamerzsjfdslkfjcscharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            print("Hello, World!");
+            print(5);
+            print(10 > 2);
+        }
+    
+        static void print(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void print(int num)
+        {
+            Console.WriteLine(num);
+        }
+        static void print(bool yesno)
+        {
+            if (yesno)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
